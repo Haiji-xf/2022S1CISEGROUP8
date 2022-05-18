e0d29aa23e44576277f137c8588581be77fcd956
SolaireTi <79572077+SolarTi@users.noreply.github.com>
2022-05-19 10:24:18 +1200

    commit to heroku 1st try

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-/*
- * @Author: Tai Zhang
- */
-require("dotenv").config({path: './.env'});
+
+require("dotenv").config({ path: './.env' });
 const express = require('express');
+
+//new added
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+//end here
 
 const connectDB = require('./config/db');
 const path = require("path");
@@ -13,15 +16,27 @@ const app = express();
 
 connectDB();
 
-app.use(cors({ origin: true, credentials: true }));
-app.use(express.json({  extended: false}));
 
-app.get('/', (req,res) => {
+
+
+
+
+app.use(cors({ origin: true, credentials: true }));
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) => {
     res.send("API is working!");
 });
 
-app.use('/api/article',article);
+app.use('/api/article', article);
 
 const port = process.env.PORT || 5555;
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./frontend/build")));
+// Step 2:
+app.get("*", function (request, response) {
+    response.sendFile(path.resolve(__dirname, "./frontend/build", "index.html"));
+});
+
+app.listen(port, () => { console.log(`Server running on port ${port}`) });
