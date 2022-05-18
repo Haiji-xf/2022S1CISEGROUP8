561a1dfd14163185ca16904de6c10a25ecab9b57
SolaireTi <79572077+SolarTi@users.noreply.github.com>
2022-05-19 10:34:22 +1200

    commit to hotfix

--- app.js
+++ app.js
@@ -2,11 +2,6 @@
 require("dotenv").config({ path: './.env' });
 const express = require('express');
 
-//new added
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-//end here
-
 const connectDB = require('./config/db');
 const path = require("path");
 var cors = require('cors');
@@ -14,12 +9,8 @@ var cors = require('cors');
 const article = require('./routes/api/article');
 const app = express();
 
+
 connectDB();
-
-
-
-
-
 
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json({ extended: false }));
@@ -32,11 +23,4 @@ app.use('/api/article', article);
 
 const port = process.env.PORT || 5555;
 
-// Step 1:
-app.use(express.static(path.resolve(__dirname, "./frontend/build")));
-// Step 2:
-app.get("*", function (request, response) {
-    response.sendFile(path.resolve(__dirname, "./frontend/build", "index.html"));
-});
-
-app.listen(port, () => { console.log(`Server running on port ${port}`) });
+app.listen(port, () => console.log(`Server running on port ${port}`));
