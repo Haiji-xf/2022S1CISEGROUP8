c5d6becc5cbc9f1ce15c1d91dc88f07a7370ad12
SolaireTi <79572077+SolarTi@users.noreply.github.com>
2022-06-01 17:18:23 +1200

    clear up main

--- frontend/src/env.js
+++
@@ -1,6 +0,0 @@
-//You cannot import file outside the src folder
-//so this env.js contains the same data with the .env in the root
-//Please make sure that change them together if needed
-const env = { "url": "http://localhost:" + (process.env.PORT || 5555) + "/api/article" }
-
-export default env;
